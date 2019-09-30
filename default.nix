6acfd9ed3d03d071094e040df879c8cbc8bcce06
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-30 17:27:52 +0000

    Make openssl-sys and libsodium build in Nix

--- default.nix
+++ default.nix
@@ -13,6 +13,12 @@ in
 
     cargoSha256 = "10jl3wkid0vsy1f6maplmcmkxgjxr75skl79phivfs82ph05ynxs";
 
+    nativeBuildInputs = [ buildPackages.perl ];
+
+    OPENSSL_STATIC = "1";
+    RUST_SODIUM_LIB_DIR = "${libsodium}/lib";
+    RUST_SODIUM_SHARED = "1";
+
     meta.platforms = lib.platforms.all;
   };
 }
