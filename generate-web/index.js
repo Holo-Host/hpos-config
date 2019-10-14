02d669f1d70253166cecc7e7af371f61f0304afb
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-10-14 17:24:12 +0000

    generate-web: format via https://standardjs.com

--- generate-web/index.js
+++ generate-web/index.js
@@ -1,25 +1,24 @@
-import { saveAs } from 'file-saver';
+import { saveAs } from 'file-saver'
 
-(async () => {
-  const { config } = await import('./pkg');
+async () => {
+  const { config } = await import('./pkg')
 
   const elements = {
     email: document.querySelector('#email'),
     form: document.querySelector('#form'),
     generate: document.querySelector('#generate'),
     password: document.querySelector('#password')
-  };
+  }
 
   elements.form.addEventListener('keydown', e => {
-    if (event.key === "Enter")
-      elements.generate.click();
-  });
+    if (event.key === 'Enter') { elements.generate.click() }
+  })
 
   elements.generate.addEventListener('click', e => {
-    const configData = config(elements.email.value, elements.password.value);
-    const configBlob = new Blob([configData.config], {type: 'application/json'});
+    const configData = config(elements.email.value, elements.password.value)
+    const configBlob = new Blob([configData.config], { type: 'application/json' })
 
-    saveAs(configBlob, 'holo-config.json');
-    alert(configData.url);
-  });
-});
+    saveAs(configBlob, 'holo-config.json')
+    alert(configData.url)
+  })
+}
