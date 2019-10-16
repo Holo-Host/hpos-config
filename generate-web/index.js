08b6b3b04c36d01e0e28bc06e4e04c477eea4e9d
PJ <pj@imagine-nyc.com>
2019-10-16 14:34:13 +0200

    code cleanup

--- generate-web/index.js
+++ generate-web/index.js
@@ -1,16 +1,3 @@
-import { saveAs } from 'file-saver'
-
-async () => {
-
-  elements.generate.addEventListener('click', e => {
-    
-    const configBlob = new Blob([configData.config], { type: 'application/json' })
-
-    saveAs(configBlob, 'holo-config.json')
-    alert(configData.url)
-  })
-}
-
 (async () => {
   const { config } = await import('./pkg')
 
@@ -35,80 +22,78 @@ async () => {
   }
 
   // Actions executed at click
-  const clickStart = () => {
-    if (!validateBrowser()) {
-      alert("Please upgrade your browser to newer version.");
-      return;
-    }
-
-    updateUiStep(1);
-  }
-
-  const clickGenerate = () => {
-    // Read inputs
-    user.email = inputs.email.value;
-    user.password = inputs.password.value;
-
-    // Check for email and pass
-    if (!validateEmail(user.email)) {
-        alert("Wrong format of email");
+  const click = {
+    start: () => {
+      if (!validateBrowser()) {
+        alert("Please upgrade your browser to newer version.");
         return;
-    } else if (!user.password) {
-        alert("Password cannot be empty");
+      }
+  
+      updateUiStep(1);
+    },
+    generate: () => {
+        // Read inputs
+      user.email = inputs.email.value;
+      user.password = inputs.password.value;
+
+      // Check for email and pass
+      if (!validateEmail(user.email)) {
+          alert("Wrong format of email");
+          return;
+      } else if (!user.password) {
+          alert("Password cannot be empty");
+          return;
+      }
+
+      // Communicate visually that something is happening in the bkgd
+      buttons.generate.disabled = true;
+      buttons.generate.innerText = "Generating...";
+
+      // Generate holo-config.json and create download blob attached to url
+      try {
+        generateDownload(user, buttons.download);
+      } catch(e) {
+        console.log(`Error executing generateDownload with an error ${e}`);
         return;
+      }
+
+      // revert
+      setTimeout(() => {
+          buttons.generate.disabled = false;
+          buttons.generate.innerText = "Generate";
+          updateUiStep(2);
+      }, 1000);
+    },
+    download: () => {
+      // Communicate visually that something is happening in the bkgd
+      buttons.download.disabled = true;
+      buttons.download.innerText = "Downloading...";
+
+      // Update user email in the UI
+      document.querySelector("#emailPlaceholder").innerText = user.email;
+
+      // revert
+      setTimeout(() => {
+          buttons.download.disabled = false;
+          buttons.download.innerText = "Download";
+          updateUiStep(3);
+      }, 1000);
+    },
+    copied: () => {
+      updateUiStep(4);
     }
-
-    // Communicate visually that something is happening in the bkgd
-    buttons.generate.disabled = true;
-    buttons.generate.innerText = "Generating...";
-
-    // Generate holo-config.json and create download blob attached to url
-    try {
-      generateDownload(user, buttons.download);
-    } catch(e) {
-      console.log(`Error executing generateDownload with an error ${e}`);
-      return;
-    }
-
-    // revert
-    setTimeout(() => {
-        buttons.generate.disabled = false;
-        buttons.generate.innerText = "Generate";
-        updateUiStep(2);
-    }, 1000);
-  }
-
-  const clickDownload = () => {
-    // Communicate visually that something is happening in the bkgd
-    buttons.download.disabled = true;
-    buttons.download.innerText = "Downloading...";
-
-    // Update user email in the UI
-    document.querySelector("#emailPlaceholder").innerText = user.email;
-
-    // revert
-    setTimeout(() => {
-        buttons.download.disabled = false;
-        buttons.download.innerText = "Download";
-        updateUiStep(3);
-    }, 1000);
-  }
-
-  const clickCopied = () => {
-    updateUiStep(4);
   }
 
   // Bind actions to buttons
-  buttons.start.onclick = clickStart;
-  buttons.generate.onclick = clickGenerate;
-  buttons.download.onclick = clickDownload;
-  buttons.copied.onclick = clickCopied;
+  buttons.start.onclick = click.start;
+  buttons.generate.onclick = click.generate;
+  buttons.download.onclick = click.download;
+  buttons.copied.onclick = click.copied;
 
   /**
    * Validate if string is email (super simple because actual validation is via sent email)
    * @param {string} email 
    */
-
   const validateEmail = (email) => {
     let re = /^\S+@\S+$/;
     return re.test(String(email).toLowerCase());
@@ -117,7 +102,6 @@ async () => {
   /**
    * Validate if browser supports required functions
    */
-
   const validateBrowser = () => {
     // Detect if browser supports download attribute on <a>
     return (typeof buttons.download.download != "undefined")
@@ -128,7 +112,6 @@ async () => {
    * 
    * @param {int} step 
    */
-
   const updateUiStep = (step) => {
     let validation = {0:!0, 1:!0, 2:!0, 3:!0, 4:!0};
 
@@ -146,8 +129,8 @@ async () => {
    * @param {Object} user 
    * @param {DomElement} button - a DomElement that will have download and attribute props updated
    */
-
   const generateDownload = (user, button) => {
+    console.log("generating...");
     const configData = config(user.email, user.password);
     const configBlob = new Blob([configData.config], { type: 'application/json' });
     const url = URL.createObjectURL(configBlob);
