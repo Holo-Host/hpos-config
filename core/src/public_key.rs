b49e92fba85286b01f5d559f65accc71e5488d56
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-29 20:56:11 +0000

    core: replace public_key::to_hcid with public_key::to_base36_id

--- core/src/public_key.rs
+++ core/src/public_key.rs
@@ -1,21 +1,13 @@
 use ed25519_dalek::PublicKey;
-use failure::Error;
-use hcid::HcidEncoding;
-use lazy_static::lazy_static;
+use failure::*;
 use url::Url;
 
-lazy_static! {
-    pub static ref HCID_CODEC: hcid::HcidEncoding =
-        HcidEncoding::with_kind("hcs0").expect("Couldn't init hcs0 hcid codec.");
+pub fn to_base36_id(public_key: &PublicKey) -> String {
+    base36::encode(&public_key.to_bytes())
 }
 
-pub fn to_hcid(public_key: &PublicKey) -> Result<String, Error> {
-    let hcid = HCID_CODEC.encode(&public_key.to_bytes())?;
-    Ok(hcid)
-}
+pub fn to_url(public_key: &PublicKey) -> Fallible<Url> {
+    let url = format!("https://{}.holohost.net", to_base36_id(&public_key));
 
-pub fn to_url(public_key: &PublicKey) -> Result<Url, Error> {
-    let hcid = to_hcid(&public_key)?;
-    let url = Url::parse(&format!("https://{}.holohost.net", hcid))?;
-    Ok(url)
+    Ok(Url::parse(&url)?)
 }
