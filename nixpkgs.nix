e7d6820f162f52b37b6c77656d4934ff43c9ee05
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-29 20:42:26 +0000

    nixpkgs: bump to 2b8a8169be0a512e93b56380d5a549bdca80dd2b

---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/2b8a8169be0a512e93b56380d5a549bdca80dd2b.tar.gz";
+  sha256 = "1i22dgixhrpfwq8k98mzli1qhzai3v4wi8f2i1x5gz3yz326048k";
+})
