7c59bb15c9d3c1c5ccb0e243184c032aff5930ed
zo-el <joelulahanna@gmail.com>
2021-07-07 18:32:58 +0530

    into-base36-id: bumpped to v2

--- into-base36-id/src/main.rs
+++ into-base36-id/src/main.rs
@@ -5,11 +5,24 @@ use hpos_config_core::*;
 use std::io::stdin;
 
 fn main() -> Fallible<()> {
-    let Config::V1 { seed, .. } = serde_json::from_reader(stdin())?;
-    let secret_key = SecretKey::from_bytes(&seed)?;
-    let public_key = PublicKey::from(&secret_key);
-
-    println!("{}", public_key::to_base36_id(&public_key));
+    match serde_json::from_reader(stdin())? {
+        Config::V1 { seed, .. } => {
+            let secret_key = SecretKey::from_bytes(&seed)?;
+            let public_key = PublicKey::from(&secret_key);
+            println!(
+                "{}",
+                public_key::holochain_pub_key_encoding(&public_key.to_bytes())
+            );
+        }
+        Config::V2 { seed, .. } => {
+            let secret_key = SecretKey::from_bytes(&seed)?;
+            let public_key = PublicKey::from(&secret_key);
+            println!(
+                "{}",
+                public_key::holochain_pub_key_encoding(&public_key.to_bytes())
+            );
+        }
+    }
 
     Ok(())
 }
