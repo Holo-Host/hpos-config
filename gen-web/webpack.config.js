1418bbdd0d6ae14846017c01d499f83f2bf52f29
zo-el <joelulahanna@gmail.com>
2021-10-22 16:52:16 +0530

    gen-web: setup build config for hcSeedBundle

--- gen-web/webpack.config.js
+++ gen-web/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
-
 const CopyPlugin = require('copy-webpack-plugin')
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -11,21 +11,39 @@ module.exports = {
     }]),
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, '.')
-    })
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './res/index.html' 
+   })
   ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|html-entities)/,
-        // include: /(node_modules|@holochain)/,
-        use: ["remove-hashbag-loader"]
-      }
-    ]
+  experiments: {
+    asyncWebAssembly: true
   },
-  resolveLoader: {
-    alias: {
-      "remove-hashbag-loader": path.join(__dirname, "./res/loaders/remove-hashbag-loader.js")
-    }
+  entry: './src/index.js',
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+  },
+  devServer: {
+      contentBase: './dist'
+  },
+  module: {
+      rules: [
+          {
+              test: /\.js$/, //using regex to tell babel exactly what files to transcompile
+              exclude: /node_modules/, // files to be ignored
+              use: {
+                  loader: 'babel-loader' // specify the loader
+              } 
+          }
+      ]
+  },
+  resolve: {
+    fallback: {
+      "path": false,
+      "crypto": false,
+      "crypto-browserify": require.resolve('crypto-browserify')
+    } 
   }
 }
