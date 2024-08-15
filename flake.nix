82b2897685acb73a8f391dcbc3d78e89dce07c7f
Stefan Junker <mail@stefanjunker.de>
2024-08-15 14:19:00 +0000

    chore(nix): configure formatter and run `nix fmt`

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "Template for Holochain app development";
 
   inputs = {
-    versions.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.45?dir=versions/weekly";
-    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.45";
+    versions.url =
+      "github:holochain/holochain/holochain-0.3.0-beta-dev.45?dir=versions/weekly";
+    versions.inputs.holochain.url =
+      "github:holochain/holochain/holochain-0.3.0-beta-dev.45";
 
     holochain-flake.url = "github:holochain/holochain";
     holochain-flake.inputs.versions.follows = "versions";
@@ -13,26 +15,20 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; }
-      {
-        systems = builtins.attrNames inputs.holochain-flake.devShells;
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = builtins.attrNames inputs.holochain-flake.devShells;
 
-        perSystem =
-          { inputs'
-          , config
-          , pkgs
-          , system
-          , ...
-          }: {
+      perSystem = { inputs', config, pkgs, system, ... }: {
+        formatter = pkgs.nixfmt-rfc-style;
 
-            devShells.default = pkgs.mkShell {
-              inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
-              packages = [
-                pkgs.nodejs-18_x
-                pkgs.binaryen
-                # more packages go here
-              ];
-            };
-          };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
+          packages = [
+            pkgs.nodejs-18_x
+            pkgs.binaryen
+            # more packages go here
+          ];
+        };
       };
+    };
 }
