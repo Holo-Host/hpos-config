d2ba03191c8a3946797f7f1d6d196e093c8bbccd
Lisa Jetton <ldjetton21@gmail.com>
2024-09-04 12:37:14 -0500

    Deploy V3 to QA Quickstart (#135) (#136)
    * sudoken: v0.0.5
    
    * sudoken: v0.0.6
    
    * hc_seed_bundle: v0.1.4
    
    * holochain: v0.1.0-rc
    
    * version bump
    
    * seed-bundle: holoport_key
    
    * set code owners
    
    * sudoken: 0.0.9
    
    * fix: quick-start-status-code
    
    * bump sodoken
    
    * bump wasm-bindgen-cli
    
    * use workspace.dependencies
    
    * bump ed25519-dalek to 2.1.1
    
    * update versions in Cargo.toml
    
    * move to flake.nix
    
    * update cargo deps
    
    * wip: v3
    
    * wip: v3
    
    * fix tests
    
    * added todos
    
    * hpos-config: tests
    
    * Interim commit with tests passing
    
    * ui tweaks and v3 updates
    
    * git ignore
    
    * pass pubkeys as bytes
    
    * fix error on multiple hps, ui tweaks, and re-enable memproof check
    
    * design review changes
    
    * pr feedback suggestions
    
    * fix package-lock issue
    
    * Added missing v3 implementation (#131)
    
    * Added missing v3 implementation
    
    * Added email getter on Config
    
    * Minimally updated gen-cli to use Clap
    
    ---------
    
    
    
    
    
    * Added missing commit for host pub key (#134)
    
    
    
    * feat: set up direnv, formatter and format flake (#133)
    
    * configure direnv
    
    * chore(nix): configure formatter and run `nix fmt`
    
    ---------
    
    Co-authored-by: alastairong1 <alastair.ong.a7o@gmail.com>
    Co-authored-by: zo-el <joelulahanna@gmail.com>
    Co-authored-by: Robbie Carlton <robbie.carlton@gmail.com>
    Co-authored-by: Alastair Ong <alastair.ong@holo.host>
    Co-authored-by: Zeeshan <zeeshan.abid.com@gmail.com>
    Co-authored-by: peeech <pj@imagine-nyc.com>
    Co-authored-by: Matthew Geddes <mg@versatus.io>
    Co-authored-by: Patrick Winfield <patrick.winfield@holo.host>
    Co-authored-by: Patrick Winfield <Paterick@users.noreply.github.com>
    Co-authored-by: Matthew Geddes <musicalcarrion@gmail.com>
    Co-authored-by: Matthew Geddes <matt.geddes@holo.host>
    Co-authored-by: Stefan Junker <1181362+steveej@users.noreply.github.com>

--- seed-bundle-explorer/tests/explorer.rs
+++ seed-bundle-explorer/tests/explorer.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use ed25519_dalek::PublicKey;
+    use ed25519_dalek::VerifyingKey;
     use hpos_config_core::Config;
     use hpos_config_seed_bundle_explorer::holoport_public_key;
 
@@ -20,10 +20,12 @@ mod tests {
         let registration_code: String = "registration-code".to_string();
         let derivation_path: String = "1".to_string();
         let device_bundle = "k6VoY3NiMJGWonB3xBCZ0R47aR6ctMScaYsrOLwRzSAAAcQY58NsOmNCDbniGsLgUhj5UoHjBrapiiDGxDGAa5Wqzm0pVuXGN106iyMHRk4dOf0iGWj65oCeB8-ZYXJdeflsVDY-DOuJaadfPZQExCyCrWRldmljZV9udW1iZXIAq2dlbmVyYXRlX2J5r3F1aWNrc3RhcnQtdjIuMA".to_string();
-        let (config, _) = Config::new_v2(
+        let rev_key: [u8; 32] = [0 as u8; 32]; // TODO: Fill this in with something
+        let (config, _) = Config::new(
             email,
             password,
             registration_code,
+            VerifyingKey::from_bytes(&rev_key).unwrap(),
             derivation_path,
             device_bundle,
             get_mock_pub_key()?,
@@ -31,11 +33,11 @@ mod tests {
         .unwrap();
         Ok(config)
     }
-    fn get_mock_pub_key() -> Result<PublicKey, String> {
+    fn get_mock_pub_key() -> Result<VerifyingKey, String> {
         let device_pub_key: String = "To4PzBU8BcVghpjGjnYjLQnP_mkT9uBJ2v969Cs7-xw".to_string();
         Ok(
             base64::decode_config(&device_pub_key, base64::URL_SAFE_NO_PAD)
-                .map(|bytes| PublicKey::from_bytes(&bytes))
+                .map(|bytes| VerifyingKey::from_bytes(&bytes[0..32].try_into().unwrap()))
                 .unwrap()
                 .unwrap(),
         )
